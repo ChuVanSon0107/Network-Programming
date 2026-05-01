d3a6c27390f1272d157dfcce9c1564e57319c4ec
ChuVanSon0107 <cvson1126034@gmail.com>
2026-05-01 10:34:14 +0700

    Add HTML response pages and implement HTTP methods in server

--- http_server_lab/server.c
+++ http_server_lab/server.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/wait.h>
-#include <errno.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 4096
@@ -13,6 +12,7 @@
 #define METHOD_LENGTH 16
 #define URI_LENGTH 256
 #define VERSION_LENGTH 32
+#define PATH_LENGTH 512
 
 const char *get_content_type(char *path) {
     if (strstr(path, ".html")) return "text/html";
@@ -21,15 +21,11 @@ const char *get_content_type(char *path) {
 }
 
 void sigchld_handler(int signo) {
-    int saved_errno = errno;
-
     // WNOHANG: Not block 
     while(waitpid(-1, NULL, WNOHANG) > 0) {}
-
-    errno = saved_errno;
 }
 
-int send_response(char *path, FILE *file, char *buffer, int connectfd, const char *format) {
+int read_and_send_response(char *path, FILE *file, char *buffer, int connectfd, const char *format) {
     /* denotes to the end of this file */
     if (fseek(file, 0, SEEK_END) != 0) {
         perror("fseek");
@@ -66,8 +62,212 @@ int send_response(char *path, FILE *file, char *buffer, int connectfd, const cha
     return 0;
 }
 
+int send_not_found_response(char *buffer, int connectfd) {
+    /* 404 Not Found */
+    char path[] = "./not-found.html";
+
+    /* open file */
+    FILE *f = fopen(path, "rb");
+    if (f == NULL) {
+        printf("Server error: Error in handing file!\n");
+        return -1;
+    }
+
+    /* send response */
+    if (read_and_send_response(path, f, buffer, connectfd, "HTTP/1.1 404 Not Found\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
+        printf("Server error: Error in sending response\n");
+        return -1;
+    }
+
+    /* close file */
+    fclose(f);
+    return 0;
+}
+
+int handle_method_not_allowed_request(char *buffer, int connectfd) {
+    // 405 Method Not Allowed
+    char path[] = "./method-not-allowed.html";
+
+    /* open file */
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        printf("Server error: Error in handing file!\n");
+        return -1;
+    }
+
+    /* send response */
+    if (read_and_send_response(path, file, buffer, connectfd, "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
+        printf("Server error: Error in sending response\n");
+        return -1;
+    }
+
+    /* close file */
+    fclose(file);
+    return 0;
+}
+
+int handle_get_request(char *path, char *buffer, int connectfd) {
+    /* check whether the file exits */
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        return send_not_found_response(buffer, connectfd);
+    } else {
+        /* send response */
+        if (read_and_send_response(path, file, buffer, connectfd, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
+            printf("Server error: Error in sending response\n");
+            return -1;
+        }
+
+        /* close file */
+        fclose(file);
+    }
+
+    return 0;
+}
+
+int handle_post_request(char *path, char *buffer, int connectfd) {
+    char *body = strstr(buffer, "\r\n\r\n");
+    if (body == NULL) {
+        printf("Request is formated wrongly!\n");
+        return -1;
+    }
+    
+    body += 4;
+    printf("Data is written into data.txt file: %s\n", body);
+    
+    /* write data to a file */
+    FILE *f = fopen("data.txt", "w");
+    if (f == NULL) {
+        printf("Server error: Error in handling file\n");
+        return -1;
+    }
+
+    fprintf(f, "%s\n", body);
+
+    /* Return message to server */
+    strcpy(path, "./post.html");
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        printf("Server error: Error in handling file!\n");
+        return -1;
+    }
+
+    /* send response */
+    if (read_and_send_response(path, file, buffer, connectfd, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
+        printf("Server error: Error in sending response\n");
+        return -1;
+    }
+
+    /* close file */
+    fclose(file);
+
+    return 0;
+}
+
+int handle_put_request(char *path, char *buffer, int connectfd) {
+    char *body = strstr(buffer, "\r\n\r\n");
+    if (body == NULL) {
+        printf("Request is formated wrongly!\n");
+        return -1;
+    }
+    body += 4;
+
+    /* write data to a file */
+    FILE *f = fopen(path, "w");
+    if (f == NULL) {
+        printf("Server error: Error in handling file\n");
+        return -1;
+    }
+    
+    fprintf(f, "%s\n", body);
+    printf("Data is updated in data.txt file: %s\n", body);
+
+    /* Return message to server */
+    strcpy(path, "./put.html");
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        printf("Server error: Error in handling file!\n");
+        return -1;
+    }
+
+    /* send response */
+    if (read_and_send_response(path, file, buffer, connectfd, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
+        printf("Server error: Error in sending response\n");
+        return -1;
+    }
+
+    /* close file */
+    fclose(file);
+
+    return 0;
+}
+
+int handle_head_request(char *path, char *buffer, int connectfd) {
+    /* open file */
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        char header[HEADER_LENGTH];
+        int header_len = snprintf(header, HEADER_LENGTH, "HTTP/1.1 404 Not Found\r\nContent-Type: %s\r\nContent-Length: 0\r\n\r\n", get_content_type(path));
+        if (send(connectfd, header, header_len, 0) == -1) {
+            perror("send");
+            return -1;
+        }
+    } else {
+        /* denotes to the end of this file */
+        if (fseek(file, 0, SEEK_END) != 0) {
+            perror("fseek");
+            return -1;
+        }
+
+        /* return the position of pointer => the size of the file */
+        long filesize = ftell(file);
+        if (filesize < 0) {
+            perror("ftell");
+            return -1;
+        }
+        
+        /* rewind to the beginning of the file */
+        rewind(file);
+
+        /* send header to client */
+        char header[HEADER_LENGTH];
+        int header_len = snprintf(header, HEADER_LENGTH, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n", get_content_type(path), filesize);
+        if (send(connectfd, header, header_len, 0) == -1) {
+            perror("send");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int handle_delete_request(char *path, char *buffer, int connectfd) {
+    /* print the name of document which is deleted */
+    printf("File need to be deleted: %s\n", path);
+
+    /* check whether the file exits */
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        return send_not_found_response(buffer, connectfd);
+    } else {
+        strcpy(path, "./delete.html");
+        FILE *file = fopen(path, "rb");
+
+        /* send response */
+        if (read_and_send_response(path, file, buffer, connectfd, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
+            printf("Server error: Error in sending response\n");
+            return -1;
+        }
+
+        /* close file */
+        fclose(file);
+    }
+
+    return 0;
+}
+ 
 void handle_request(char *buffer, int connectfd) {
-    char method[METHOD_LENGTH], uri[URI_LENGTH], version[VERSION_LENGTH], path[512];
+    char method[METHOD_LENGTH], uri[URI_LENGTH], version[VERSION_LENGTH], path[PATH_LENGTH];
 
     while(1) {
         /* receive HTTP request from client */
@@ -85,63 +285,26 @@ void handle_request(char *buffer, int connectfd) {
         /* parse HTTP request */
         sscanf(buffer, "%15s %255s %31s", method, uri, version);
 
-        if (strcmp(method, "GET") == 0) {
-            /* path to resource */
-            if (strcmp(uri, "/") == 0) {
-                strcpy(path, "./index.html");
-            } else {
-                snprintf(path, sizeof(path), ".%s", uri);
-            }
-
-            /* check whether the file exits */
-            FILE *file = fopen(path, "rb");
-            if (file == NULL) {
-                /* 404 Not Found */
-                strcpy(path, "./not-found.html");
-
-                /* open file */
-                FILE *f = fopen(path, "rb");
-                if (f == NULL) {
-                    printf("Server error: Error in handing file!\n");
-                    return;
-                }
-
-                /* send response */
-                if (send_response(path, f, buffer, connectfd, "HTTP/1.1 404 Not Found\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
-                    printf("Server error: Error in sending response\n");
-                }
-
-                /* close file */
-                fclose(f);
-
-            } else {
-                /* send response */
-                if (send_response(path, file, buffer, connectfd, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
-                    printf("Server error: Error in sending response\n");
-                }
-
-                /* close file */
-                fclose(file);
-            }
-
+        /* path to resource */
+        if (strcmp(uri, "/") == 0) {
+            strcpy(path, "./index.html");
         } else {
-            // 405 Method Not Allowed
-            strcpy(path, "./method-not-allowed.html");
+            snprintf(path, sizeof(path), ".%s", uri);
+        }
 
-            /* open file */
-            FILE *file = fopen(path, "rb");
-            if (file == NULL) {
-                printf("Server error: Error in handing file!\n");
-                return;
-            }
-
-            /* send response */
-            if (send_response(path, file, buffer, connectfd, "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n") == -1) {
-                printf("Server error: Error in sending response\n");
-            }
-
-            /* close file */
-            fclose(file);
+        /* handle request */
+        if (strcmp(method, "GET") == 0) {
+            handle_get_request(path, buffer, connectfd);
+        } else if (strcmp(method, "POST") == 0) {
+            handle_post_request(path, buffer, connectfd);
+        } else if (strcmp(method, "PUT") == 0) {
+            handle_put_request(path, buffer, connectfd);
+        } else if (strcmp(method, "HEAD") == 0) {
+            handle_head_request(path, buffer, connectfd);
+        } else if (strcmp(method, "DELETE") == 0) {
+            handle_delete_request(path, buffer, connectfd);
+        } else {
+            handle_method_not_allowed_request(buffer, connectfd);
         }
     }
 }
